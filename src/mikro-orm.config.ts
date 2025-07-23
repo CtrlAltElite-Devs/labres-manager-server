11b4a6702f7cde593f4c44000adcd701d541f662
Yander <lorenzolubguban@gmail.com>
2025-07-23 09:27:40 +0800

    refactor: simplify SSL connection handling in MikroORM config based on database type

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -4,17 +4,23 @@ import { Migrator } from "@mikro-orm/migrations";
 import "dotenv/config";
 import { SeedManager } from "@mikro-orm/seeder";
 
+const isNeon = process.env.DATABASE_URL?.includes('neon.tech');
+
 export default defineConfig({
   driver: PostgreSqlDriver,
   clientUrl: process.env.DATABASE_URL!,
   entities: entities,
   extensions: [Migrator, SeedManager],
   driverOptions: {
-    connection: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      connection: isNeon
+        ? {
+            ssl: {
+              rejectUnauthorized: false, // required for Neon
+            },
+          }
+        : {
+            ssl: false, // for local
+          },
     },
-  },
   debug: true,
 });
