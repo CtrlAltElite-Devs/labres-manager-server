cacd60d4969363aef6c33b3b97b21eb7f6252da8
Yander <lorenzolubguban@gmail.com>
2025-08-24 14:20:43 +0800

    feat: (configuration) add cross-env for environment variable management and update CORS logic

--- src/configurations/bootstrap-configuration.ts
+++ src/configurations/bootstrap-configuration.ts
@@ -10,9 +10,9 @@ export function ApplyConfigurations(app: INestApplication<any>){
 }
 
 export function ApplyCorsConfigurations(app: INestApplication<any>){
-  const isStaging = process.env.APP_ENV === 'staging';
-  console.log("isStaging: ", isStaging);
-  const corsOrigins = !isStaging
+  const isDevOrStaging = process.env.APP_ENV === "staging" || process.env.NODE_ENV === "development";
+  console.log("isDevelopmentOrStaging: ", isDevOrStaging);
+  const corsOrigins = !isDevOrStaging
     ? ['https://your-production-frontend.com']
     : true
 
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
+import "dotenv/config";
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ApplyConfigurations, ApplyCorsConfigurations } from './configurations/bootstrap-configuration';
-import "dotenv/config";
 import { ApplyApiVersioning } from './configurations/api-versioning-configuration';
 import { UseSwagger } from './configurations/swagger-configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   if (process.env.APP_ENV === "staging" || process.env.NODE_ENV === "development") {
     UseSwagger(app);
   }
-
-
   ApplyConfigurations(app);
   ApplyCorsConfigurations(app);
   ApplyApiVersioning(app);
