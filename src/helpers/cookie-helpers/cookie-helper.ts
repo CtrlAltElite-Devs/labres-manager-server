8109ecdb67c5c1b5e5b37900478659fb796aa6ff
y4nder <lorenzolubguban@gmail.com>
2025-09-10 23:18:29 +0800

    fix: added environment based cookie configurations

--- src/helpers/cookie-helpers/cookie-helper.ts
+++ src/helpers/cookie-helpers/cookie-helper.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+import { IS_DEV_OR_STAGING } from "src/utils/environment";
 
 export type AuthenticationTokens = {
     token: string;
@@ -6,31 +7,39 @@ export type AuthenticationTokens = {
 }
 
 export class CookieHelpers {
-    static SetTokens(response: Response, tokens: AuthenticationTokens){
+    static SetTokens(response: Response, tokens: AuthenticationTokens) {
         const { token, refreshToken } = tokens;
+
+        const isProd = !IS_DEV_OR_STAGING;
+
         response.cookie('refreshToken', refreshToken, {
             httpOnly: true,
-            secure: true,
-            sameSite: 'lax',
-            path: '/api/v1/auth/refresh'
+            secure: isProd,                 // required for SameSite=None
+            sameSite: isProd ? 'none' : 'lax',
+            path: '/api/v1/auth/refresh',
         });
 
         response.cookie('token', token, {
             httpOnly: true,
-            secure: true,
-            sameSite: 'lax',
+            secure: isProd,
+            sameSite: isProd ? 'none' : 'lax',
         });
     }
 
-    static RemoveTokens(response: Response){
+    static RemoveTokens(response: Response) {
+        const isProd = !IS_DEV_OR_STAGING;
+
         response.clearCookie('refreshToken', {
             httpOnly: true,
-            secure: true,
-            path: '/', 
+            secure: isProd,
+            sameSite: isProd ? 'none' : 'lax',
+            path: '/api/v1/auth/refresh',
         });
+
         response.clearCookie('token', {
             httpOnly: true,
-            secure: true,
+            secure: isProd,
+            sameSite: isProd ? 'none' : 'lax',
             path: '/',
         });
     }
