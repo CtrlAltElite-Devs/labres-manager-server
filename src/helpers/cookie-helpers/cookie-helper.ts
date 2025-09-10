750887dc6a3cd950ff9d03c6141be172841bf528
y4nder <lorenzolubguban@gmail.com>
2025-09-10 23:31:08 +0800

    fix: used proper conditioning

--- src/helpers/cookie-helpers/cookie-helper.ts
+++ src/helpers/cookie-helpers/cookie-helper.ts
@@ -1,5 +1,5 @@
 import { Response } from "express";
-import { IS_DEV_OR_STAGING } from "src/utils/environment";
+import { IS_PROD_OR_STAGING } from "src/utils/environment";
 
 export type AuthenticationTokens = {
     token: string;
@@ -10,36 +10,32 @@ export class CookieHelpers {
     static SetTokens(response: Response, tokens: AuthenticationTokens) {
         const { token, refreshToken } = tokens;
 
-        const isProd = !IS_DEV_OR_STAGING;
-
         response.cookie('refreshToken', refreshToken, {
             httpOnly: true,
-            secure: isProd,                 // required for SameSite=None
-            sameSite: isProd ? 'none' : 'lax',
+            secure: IS_PROD_OR_STAGING,                 // required for SameSite=None
+            sameSite: IS_PROD_OR_STAGING ? 'none' : 'lax',
             path: '/api/v1/auth/refresh',
         });
 
         response.cookie('token', token, {
             httpOnly: true,
-            secure: isProd,
-            sameSite: isProd ? 'none' : 'lax',
+            secure: IS_PROD_OR_STAGING,
+            sameSite: IS_PROD_OR_STAGING ? 'none' : 'lax',
         });
     }
 
     static RemoveTokens(response: Response) {
-        const isProd = !IS_DEV_OR_STAGING;
-
         response.clearCookie('refreshToken', {
             httpOnly: true,
-            secure: isProd,
-            sameSite: isProd ? 'none' : 'lax',
+            secure: IS_PROD_OR_STAGING,
+            sameSite: IS_PROD_OR_STAGING ? 'none' : 'lax',
             path: '/api/v1/auth/refresh',
         });
 
         response.clearCookie('token', {
             httpOnly: true,
-            secure: isProd,
-            sameSite: isProd ? 'none' : 'lax',
+            secure: IS_PROD_OR_STAGING,
+            sameSite: IS_PROD_OR_STAGING ? 'none' : 'lax',
             path: '/',
         });
     }
--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,1 +1,2 @@
 export const IS_DEV_OR_STAGING = process.env.APP_ENV === "staging" || process.env.NODE_ENV === "development"
+export const IS_PROD_OR_STAGING = process.env.APP_ENV === "staging" || process.env.NODE_ENV === "production";
