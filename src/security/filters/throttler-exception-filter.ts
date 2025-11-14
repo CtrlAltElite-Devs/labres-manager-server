139b2dcecf3529bb615716ecaf2a47e70f9c4774
y4nder <lorenzolubguban@gmail.com>
2025-11-14 21:48:40 +0800

    hotfix(auth) : remove cookies on rate limit exception

--- src/security/filters/throttler-exception-filter.ts
+++ src/security/filters/throttler-exception-filter.ts
@@ -1,12 +1,20 @@
 import { Catch, ExceptionFilter, ArgumentsHost } from '@nestjs/common';
 import { ThrottlerException } from '@nestjs/throttler';
 import { Response } from 'express';
+import { CookieHelpers } from 'src/helpers/cookie-helpers/cookie-helper';
+import { AuthenticatedRequest } from '../common/application-requests';
 
 @Catch(ThrottlerException)
 export class ThrottlerFilter implements ExceptionFilter {
   catch(exception: ThrottlerException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
+    const request = ctx.getRequest<AuthenticatedRequest>();
     const response = ctx.getResponse<Response>();
+
+    // Clear auth cookies
+    CookieHelpers.RemoveTokens(response, {
+      isAdmin: request.admin !== undefined
+    });
 
     return response.status(429).json({
         statusCode: 429,
