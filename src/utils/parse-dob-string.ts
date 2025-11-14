2adea39809d2eb804be5bed1fa148f09f8103bd5
y4nder <lorenzolubguban@gmail.com>
2025-11-14 16:48:37 +0800

    hotfix: dob parser

--- src/utils/parse-dob-string.ts
+++ src/utils/parse-dob-string.ts
@@ -2,5 +2,6 @@ export function parseDobString(dob: string): Date {
     const year = parseInt(dob.slice(0, 4), 10);
     const month = parseInt(dob.slice(4, 6), 10);
     const day = parseInt(dob.slice(6, 8), 10);
-    return new Date(year, month - 1, day);
+    
+    return new Date(Date.UTC(year, month - 1, day));
 }
