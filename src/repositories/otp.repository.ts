1e25ede4abfbcaa6c0001a38c8cb6e66aa1c7db9
Leander Lubguban <113151776+y4nder@users.noreply.github.com>
2025-11-15 02:34:20 +0800

    feat (auth) : otp email integration (#29)

--- src/repositories/otp.repository.ts
+++ src/repositories/otp.repository.ts
@@ -1,6 +1,24 @@
-import { EntityRepository } from "@mikro-orm/core";
-import { OTP } from "src/entities/security/otp.entity";
+import { EntityRepository } from '@mikro-orm/core';
+import { OTP } from 'src/entities/security/otp.entity';
 
-export class OTPRepository extends EntityRepository<OTP>{
+export class OTPRepository extends EntityRepository<OTP> {
+  async CreateOTP(email: string) {
+    const otp = new OTP();
+    otp.code = this.generateOTP();
+    otp.email = email;
 
+    await this.upsert(otp, {
+      onConflictFields: ['email'],
+      onConflictAction: 'merge',
+      onConflictExcludeFields: ['id'],
+    });
+
+    return otp;
+  }
+
+  private generateOTP(): string {
+    return Math.floor(10000 * Math.random())
+      .toString()
+      .padStart(4, '0');
+  }
 }
